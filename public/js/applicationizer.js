9ceedfcd3dfc372753870e0be3fd1ac80dff4474
Elad Nava <eladnava@gmail.com>
2016-04-21 01:21:23 +0300

    Redesign the Applicationize form to support customization

--- public/js/applicationizer.js
+++ public/js/applicationizer.js
@@ -1,34 +1,45 @@
-$(document).ready(function() { 
+$(document).ready(function () {
     "use strict";
-    
+
+    // Handle advanced options toggle click
+    $('.advanced-options-toggle').click(function (e) {
+        // Cancel default browser navigation
+        e.preventDefault();
+        
+        // Make sure we're not currently generating an extension
+        if (!$(this).find('.submit').is(':disabled')) {
+            // Toggle the advanced options container's visibility
+            $('.advanced-options-container').toggle();
+        }
+    });
+
     // Handle applicationize form submit
-    $('.applicationize-form').submit(function(e)
-    {
+    $('.applicationize-form').submit(function (e) {
         // Get input URL
         var url = $(this).find('input[name="url"]').val();
-        
+
         // No URL?
         if (!url) {
             alert('Please enter a valid web app URL to applicationize.');
             return e.preventDefault();
         }
-        
+
         // Verify protocol
         if (url.substring(0, 4) !== 'http') {
             alert('Please provide a valid web app URL.\r\nExample: https://www.messenger.com/');
             return e.preventDefault();
         }
-        
+
         // Grab spinner and submit elements
         var submit = $(this).find('.submit');
         var spinner = $(this).find('.spinner');
-        
+
         // Hide submit button and show spinner
         submit.hide();
         spinner.show();
-        
+
         // Wait a few seconds (ugly hack since we don't know when the file has been downloaded)
-        setTimeout(function() {
+        setTimeout(function () {
             // Flip the visibility back to normal
             spinner.hide();
             submit.show();
