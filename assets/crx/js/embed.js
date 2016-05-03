2638bce63374256d0a2486119d0e3a2383d2577b
Elad Nava <eladnava@gmail.com>
2016-05-03 23:30:53 +0300

    Make it possible to download files within applicationized web apps
    Closes #16.

--- assets/crx/js/embed.js
+++ assets/crx/js/embed.js
@@ -1,12 +1,18 @@
 // Wait for embed.html page DOM to load
-window.onload = function() {
+window.onload = function () {
     // Get webview DOM element
     var webview = document.getElementById('webview');
 
     // Sign up for "newwindow" event
     // Emitted when a target="_blank" link is clicked within the webview
-    webview.addEventListener('newwindow', function(e) {
+    webview.addEventListener('newwindow', function (e) {
         // Open the link in a new browser tab/window
         window.open(e.targetUrl);
+    });
+
+    // Sign up for 'permissionrequest' event
+    webview.addEventListener('permissionrequest', function (e) {
+        // Allow all permission requests
+        e.request.allow();
     });
 }
