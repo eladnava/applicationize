e8627b91d3c573af007e1d725ac291ff427df2a8
Elad Nava <eladnava@gmail.com>
2016-06-01 01:11:03 +0300

    Support for popup windows within applicationized apps
    Thanks to @markmcintyre for reporting in #18!

--- assets/crx/js/background.js
+++ assets/crx/js/background.js
@@ -24,5 +24,45 @@ chrome.app.runtime.onRestarted.addListener(function () {
  */
 function runApp() {
     // Creat a new Chrome app window
-    chrome.app.window.create('html/embed.html', {inject-app-window-params});
+    chrome.app.window.create('html/embed.html', {inject-app-window-params}, onWindowLoaded());
+}
+
+/**
+ * Called before the contentWindow's onload event
+ *
+ * @see http://developer.chrome.com/apps/app.window.html
+ */
+function onWindowLoaded(popup) {
+    return function (win) {
+        // On window loaded event
+        win.contentWindow.onload = function () {
+            // Get webview 
+            var webview = win.contentWindow.document.getElementById('webview');
+
+            // Sign up for 'permissionrequest' event
+            webview.addEventListener('permissionrequest', function (e) {
+                // Allow all permission requests
+                e.request.allow();
+            });
+
+            // Sign up for 'newwindow' event
+            // Emitted when a target='_blank' link is clicked within the webview
+            webview.addEventListener('newwindow', function (e) {
+                // Popup?
+                if (e.initialWidth > 0 && e.initialHeight > 0) {
+                    // Open it in a popup window with a set width and height
+                    return chrome.app.window.create('html/embed.html', { frame: { type: 'chrome' }, innerBounds: { width: e.initialWidth, height: e.initialHeight } }, onWindowLoaded(e));
+                }
+
+                // Open the link in a new browser tab/window
+                win.contentWindow.open(e.targetUrl);
+            });
+
+            // Is this a popup window?
+            if (popup) {
+                // Override webview source with popup's target URL
+                webview.src = popup.targetUrl;
+            }
+        };
+    };
 }
