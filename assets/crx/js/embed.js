f7282c17f8a91833a8699a36db636b84eb6eba82
Elad <eladnava@gmail.com>
2017-06-28 20:05:09 +0300

    Support for Ctrl key on Windows for special shortcuts
    Fixes #46.

--- assets/crx/js/embed.js
+++ assets/crx/js/embed.js
@@ -10,13 +10,16 @@ var zoomFactor = 1.0;
 
 // Listen to keydown event
 document.addEventListener('keydown', function (e) {
-    // Refresh the page (CMD + R)
-    if (e.metaKey && e.keyCode == 'R'.charCodeAt(0)) {
+    // Check whether CTRL on Windows or CMD on Mac is pressed
+    var modifierActive = (navigator.platform.startsWith('Win')) ? e.ctrlKey : e.metaKey;
+
+    // Refresh the page (CTRL/CMD + R)
+    if (modifierActive && e.keyCode == 'R'.charCodeAt(0)) {
         webview.reload();
     }
 
-    // Find in page (CMD + F)
-    if (e.metaKey && e.keyCode == 'F'.charCodeAt(0)) {
+    // Find in page (CTRL/CMD + F)
+    if (modifierActive && e.keyCode == 'F'.charCodeAt(0)) {
         // Show the find box
         findBox.style.display = 'block';
 
@@ -27,20 +30,20 @@ document.addEventListener('keydown', function (e) {
         findInput.select();
     }
 
-    // Copy URL (CMD + L)
-    if (e.metaKey && e.keyCode == 'L'.charCodeAt(0)) {
+    // Copy URL (CTRL/CMD + L)
+    if (modifierActive && e.keyCode == 'L'.charCodeAt(0)) {
         // Copy webview source to clipboard
         copyToClipboard(webview.src, 'text/plain');
     }
 
-    // Zoom in (CMD +)
-    if (e.metaKey && e.keyCode == 187) {
+    // Zoom in (CTRL/CMD +)
+    if (modifierActive && e.keyCode == 187) {
         zoomFactor += 0.1;
         webview.setZoom(zoomFactor);
     }
 
-    // Zoom out (CMD -)
-    if (e.metaKey && e.keyCode == 189) {
+    // Zoom out (CTRL/CMD -)
+    if (modifierActive && e.keyCode == 189) {
         zoomFactor -= 0.1;
 
         // Don't let zoom drop below 0.2
@@ -51,8 +54,8 @@ document.addEventListener('keydown', function (e) {
         webview.setZoom(zoomFactor);
     }
 
-    // Reset zoom (CMD + 0)
-    if (e.metaKey && e.keyCode == '0'.charCodeAt(0)) {
+    // Reset zoom (CTRL/CMD + 0)
+    if (modifierActive && e.keyCode == '0'.charCodeAt(0)) {
         zoomFactor = 1.0;
         webview.setZoom(zoomFactor);
     }
