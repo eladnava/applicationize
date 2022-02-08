44b0adb340fafd791bffd165439209502c1df99b
Elad Nava <eladnava@gmail.com>
2022-02-07 22:19:22 -0500

    Built-in HTTPS support

--- server.js
+++ server.js
@@ -1,19 +1,22 @@
+var fs = require('fs');
 var koa = require('koa');
+var http = require('http');
+var https = require('https');
 var route = require('koa-route');
 var static = require('koa-static');
 var body = require('koa-better-body');
 
 // Koa middleware
-var https = require('./lib/middleware/https');
 var error = require('./lib/middleware/error');
+var redirect = require('./lib/middleware/redirect');
 
 // Create koa app
 var app = koa();
 
 // Koa middleware
 app.use(error());
-app.use(https());
 app.use(body());
+app.use(redirect());
 app.use(static('./public'));
 
 // HTML file aliases
@@ -24,9 +27,16 @@ app.use(route.post('/applicationize', require('./lib/routes/applicationize')));
 
 // Define configurable port
 var port = process.env.PORT || 8080;
+var securePort = process.env.SECURE_PORT || 8443;
 
 // Listen for connections
-app.listen(port);
+http.createServer(app.callback()).listen(port);
+
+// HTTPS support
+https.createServer({
+    key: fs.readFileSync('ssl/server.key'),
+    cert: fs.readFileSync('ssl/server.crt')
+}, app.callback()).listen(securePort);
 
 // Log port
 console.log('Server listening on port ' + port);
